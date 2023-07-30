1fbaf2e3fd594e69a6e252eb096d4372d76052bf
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-07-30 23:47:36 +0545

    File name changed from pie to pieChart

--- pieChart.py
+++ pieChart.py
@@ -1,7 +1,10 @@
 from matplotlib import pyplot as plt
 
-plt.style.use("fivethirtyeight")
+#plt.style.use("fivethirtyeight")
 
+slices  = [60, 40]
+
+plt.pie(slices)
 plt.title("My Pie Chart")
 plt.tight_layout()
 plt.show()
