54070a23f140526178491c94082ffb63a6751f1a
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-07-31 23:50:12 +0545

    Basics of pie chart done.

--- pieChart.py
+++ pieChart.py
@@ -2,9 +2,12 @@ from matplotlib import pyplot as plt
 
 plt.style.use("fivethirtyeight")
 
-slices  = [60, 40]
+slices  = [120, 80, 30, 20] # Don't have to add up to 100
+labels = ['Sixty', 'Forty', 'Extra1', 'Extra2']
+#colors = ['blue', 'red', 'yellow', 'green']
+colors = ['#008fd5', '#fc4f30', '#e5ae37', '#6d904f']
 
-plt.pie(slices)
+plt.pie(slices, labels=labels, colors=colors, wedgeprops={'edgecolor': 'black'})
 plt.title("My Pie Chart")
 plt.tight_layout()
 plt.show()
