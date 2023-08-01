7fa19dd3b5b674dcf58a9f7ccd5a133bc0a46498
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-08-01 22:15:35 +0545

    Pir chart from real data.

--- pieChart.py
+++ pieChart.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 
+# Learn to see all available styles
 plt.style.use("fivethirtyeight")
 
 slices  = [120, 80, 30, 20] # Don't have to add up to 100
@@ -12,3 +13,22 @@ plt.title("My Pie Chart")
 plt.tight_layout()
 plt.show()
 
+# Another chart
+# Language Popularity
+slices = [59219, 55466, 47544, 36443, 35917, 31991, 27097, 23030, 20524, 18523, 18017, 7920, 7331, 7201, 5833]
+labels = ['JavaScript', 'HTML/CSS', 'SQL', 'Python', 'Java', 'Bash/Shell/PowerShell', 'C#', 'PHP', 'C++', 
+          'TypeScript', 'C', 'Other(s):', 'Ruby', 'Go', 'Assembly']
+
+plt.pie(slices, labels=labels, wedgeprops={'edgecolor' : 'black'})
+plt.title("Language Popularity")
+plt.tight_layout()
+# plt.legend()
+plt.show()
+
+# Not a good idea to use pie chart when there is so many data, at most 
+# 5 would be better.
+
+slices = [59219, 55466, 47544, 36443, 35917]
+labels = ['JavaScript', 'HTML/CSS', 'SQL', 'Python', 'Java']
+plt.pie(slices, labels=labels, wedgeprops={'edgecolor' : 'black'})
+plt.show()
