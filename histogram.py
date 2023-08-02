aac87338d07f6bce9786d651172529c42ae46e83
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-08-02 23:43:04 +0545

    Histogram with self defined bin, and how to read it.

--- histogram.py
+++ histogram.py
@@ -12,6 +12,13 @@ ages = [18, 19, 21, 25, 26, 26, 30, 32, 38, 45, 55]
 # color = '#fc4f30'
 # plt.legend()
 
+# you can pass bins as a list as well
+# to exclude just remove bin, e.g to remove ages < 20
+# just remove bin 10
+bins = [10, 20, 30, 40, 50, 60, 70]
+
+plt.hist(ages, bins=bins, edgecolor='black') # bins = 5 divide the data into 
+# 5 beans, and plot it.
 plt.title('Ages of Respondents')
 plt.xlabel('Ages')
 plt.ylabel('Total Respondents')
