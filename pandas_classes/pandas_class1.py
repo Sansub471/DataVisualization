46cdc62e48a49f25525bc8b14f6620cc89490012
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-11-17 13:09:43 +0545

    the name of the day is here.

--- pandas_classes/pandas_class1.py
+++ pandas_classes/pandas_class1.py
@@ -1,1 +1,10 @@
 import pandas as pd
+
+# This is the name of the day for me and my people and I make it very possible for the name of the source
+# tomorrow the name of the person will be there for me to be okay
+# I will take it easy for people like me to 
+# Make it happen and over the years 
+# Let's take it to the top of the game 
+# I want to make it happend and see how it goes
+# My name is also working here
+# I had the name of the week and let's see where this gets me 
