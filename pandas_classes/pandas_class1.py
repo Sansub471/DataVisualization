108f5cc10713a5dc2fea4f46ca62f79927839bdc
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-11-18 23:08:40 +0545

    Comments removed.

--- pandas_classes/pandas_class1.py
+++ pandas_classes/pandas_class1.py
@@ -1,11 +1,2 @@
 import pandas as pd
 
-# Enumerating objects: 7, done.
-# Counting objects: 100% (7/7), done.
-# Delta compression using up to 8 threads
-# Compressing objects: 100% (4/4), done.
-# Writing objects: 100% (4/4), 632 bytes | 632.00 KiB/s, done.
-# Total 4 (delta 1), reused 0 (delta 0), pack-reused 0
-# remote: Resolving deltas: 100% (1/1), completed with 1 local object.
-# To github.com:Sansub471/DataVisualization.git
-#    fd47050..46cdc62  main -> main
