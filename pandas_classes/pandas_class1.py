08869b3fa88c9689541ebe8ec7d15a9f1adbe0f1
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-11-18 23:08:19 +0545

    The day will start from here.

--- pandas_classes/pandas_class1.py
+++ pandas_classes/pandas_class1.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-# This is the name of the day for me and my people and I make it very possible for the name of the source
-# tomorrow the name of the person will be there for me to be okay
-# I will take it easy for people like me to 
-# Make it happen and over the years 
-# Let's take it to the top of the game 
-# I want to make it happend and see how it goes
-# My name is also working here
-# I had the name of the week and let's see where this gets me 
+# Enumerating objects: 7, done.
+# Counting objects: 100% (7/7), done.
+# Delta compression using up to 8 threads
+# Compressing objects: 100% (4/4), done.
+# Writing objects: 100% (4/4), 632 bytes | 632.00 KiB/s, done.
+# Total 4 (delta 1), reused 0 (delta 0), pack-reused 0
+# remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+# To github.com:Sansub471/DataVisualization.git
+#    fd47050..46cdc62  main -> main
