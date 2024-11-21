cc5d95acd2432ee18776b2963fd8bcbc2ffff209
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-11-21 22:04:18 +0545

    print lines removed

--- pandas_classes/pandas_class1.py
+++ pandas_classes/pandas_class1.py
@@ -1,12 +1,2 @@
 import pandas as pd
 
-import numpy as np
-import pandas as pd
-
-# 1. Generate random data using NumPy
-np.random.seed(42)
-data = np.random.randint(1, 101, (10, 3))  # 10 rows, 3 columns of random integers between 1 and 100
-
-# 2. Create a pandas DataFrame
-df = pd.DataFrame(data, columns=["Math", "Science", "English"])
-
