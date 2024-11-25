7de92532e1b27f7badda49c3ee5d1cee81775766
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-11-25 22:48:41 +0545

    Majority code converted from C++

---
+++ pandas_classes/majorityElement.py
@@ -0,0 +1,40 @@
+from typing import List
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        candidate1, candidate2 = 0, 1
+        count1, count2 = 0, 0
+        result = []
+        n = len(nums)
+        
+        # First pass to find potential candidates
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1 = num
+                count1 = 1
+            elif count2 == 0:
+                candidate2 = num
+                count2 = 1
+            else:
+                count1 -= 1
+                count2 -= 1
+        
+        # Second pass to verify the candidates
+        count1, count2 = 0, 0
+        for num in nums:
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+        
+        # Check if candidates appear more than n/3 times
+        if count1 > n // 3:
+            result.append(candidate1)
+        if count2 > n // 3:
+            result.append(candidate2)
+        
+        return result
